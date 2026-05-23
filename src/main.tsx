f2fea825ca36f91ed7e591b1fb45658526e9ab94
nevy11 <smongare2004@gmail.com>
2026-05-23 09:05:26 +0300

    import css issue in build mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./styles.css";
 import { createRoot } from "react-dom/client";
 import { getRouter } from "./router";
 import { RouterProvider } from "@tanstack/react-router";
