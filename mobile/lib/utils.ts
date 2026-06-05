f820d24bc66a2fa0339b30b7b89b724b40a4e0b3
Nevy11 <smongare2004@gmail.com>
2026-06-05 09:43:02 +0300

    remove mobile folder

--- mobile/lib/utils.ts
+++
@@ -1,5 +0,0 @@
-import * as Crypto from "expo-crypto";
-
-export async function hashPin(pin: string): Promise<string> {
-  return await Crypto.digestStringAsync(Crypto.CryptoDigestAlgorithm.SHA256, pin);
-}
